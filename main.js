d163f5d3c2184af762601102a41a3787297838e8
Angel Afube <angel@zestpayment.com>
2024-06-27 08:09:50 +0100

    develop a two-page-portfolio-prototype

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+  
+    menuButton.addEventListener('click', function() {
+      mobileMenu.classList.toggle('hidden');
+    });
+  });
+  
+  document.getElementById('current-year').textContent = new Date().getFullYear();
